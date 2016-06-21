93f2a3674c022bcafa7ce569596bdc59c6ed0dd5
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-06-21 09:56:06 +0600

    Updated file structure

--- app/app.js
+++ app/app.js
@@ -2,13 +2,21 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    'ngRoute'
+]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    //$locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    $routeProvider.otherwise({redirectTo: '/'});
+}])
+    .config([
+        '$routeProvider',
+        function ($routes) {
+            $routes.when('/', {
+                templateUrl: 'components/changelist/changelist.html',
+                controller: 'ChangelistController'
+            });
+            $routes.otherwise({
+                redirectTo: '/'
+            });
+        }
+    ]);
